2043c481ac6761bf0c6b6e6146dba311b099197d
Sosolaiser <raylaiser@gmail.com>
2025-01-04 12:12:20 +0300

    java code

---
+++ OOP Exercise/Employee.java
@@ -0,0 +1,26 @@
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private int monthSalary;
+    public Employee(String firstName,String lastName,int monthSalary){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        if(monthSalary < 0){
+            this.monthSalary = 0;
+        }
+        else{
+            this.monthSalary = monthSalary;
+        }
+
+    }
+    public void salaryRaise(int percent){
+        this.monthSalary = this.monthSalary + this.monthSalary * percent / 100;
+    }
+    public int yearSalary(){
+        return this.monthSalary * 12;
+    }
+    public String employeeInf(){
+        return String.format("Employee First name:%s\nEmployee Last name:%s\nEmployee Year salary:%,d\n\n",this.firstName,this.lastName,yearSalary());
+        
+    }
+}
